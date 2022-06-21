428043b1ca811dec2c16a1182e6d71d5b633ac57
BrunoTaufner <brunotaufner08@gmail.com>
2022-06-20 21:38:34 -0300

    Problems Solution

--- 1008.py
+++ 1008.py
@@ -1,4 +1,4 @@
 print('NUMBER =', int(input()))
 workedHours = int(input())
 salary = float(input())
-print('SALARY = U$', round(workedHours*salary), 2)
+print('SALARY = U$', '{:.2f}'.format(workedHours*salary))
